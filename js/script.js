7c2d5604cac71fa3abfefd5b9023079fdfb1acb5
Jhonchuri11 <jhon.churivanti@tecsup.edu.pe>
2024-02-07 07:32:48 -0500

    Second commit, filtros actualizados a avanzados

--- js/script.js
+++ js/script.js
@@ -8,3 +8,38 @@ showSearchButton.addEventListener('click', function() {
         anotherSearchSection.style.display = 'none';
     }
 });
+
+function cambioEstado() {
+    var button = document.getElementById("showSearch");
+    if (button.innerHTML === "Mostrar filtros") {
+        button.innerHTML = "Ocultar filtros";
+    } else {
+        button.innerHTML = "Mostrar filtros";
+    }
+}
+
+// Funcionalidad para el agregado y eliminado de contenidos de filtros
+function agregarelemento() {
+    // Primero clonamos el primer elemento y agregamos lo nuevo como clon al contenedor
+    var contenedor = document.getElementById("contenedor");
+    var elementoOriginal = contenedor.querySelector('.elemento');
+    var nuevoElemento = elementoOriginal.cloneNode(true);
+    contenedor.appendChild(nuevoElemento);
+
+    // Limpiamos el input de nuevo elemento
+    var inputs = nuevoElemento.querySelectorAll('input');
+    inputs.forEach(function(input) {
+        input.value = '';
+    });
+}
+
+// Funcionalidad para eliminar o quitar elemento de contenidos de filtros
+function eliminarElemento(row) {
+    var contenedor = document.getElementById("contenedor");
+    var elemento = contenedor.getElementsByClassName("elemento");
+    
+    if (elemento.length > 1) {
+        var elementoPadre = row.parentNode.parentNode.parentNode;
+        contenedor.removeChild(elementoPadre);
+    }
+}
